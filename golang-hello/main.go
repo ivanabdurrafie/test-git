673c6fba6eaae48869c0ea777acacad3f19e1729
ivanabdurrafie <iabdurrafie48@gmail.com>
2021-10-04 14:36:28 +0700

    add variable

--- golang-hello/main.go
+++ golang-hello/main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("this is hello world")
+
+	var firstName string = "ivan"
+	lastName := "abdurrafie"
+
+	fmt.Printf("hallo %s %s! \n ", firstName, lastName)
 }
