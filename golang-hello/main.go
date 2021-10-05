632187f7b355eb9dba679e346a1b6d4da4042144
ivanabdurrafie <iabdurrafie48@gmail.com>
2021-10-05 22:11:46 +0700

    add array

--- golang-hello/main.go
+++ golang-hello/main.go
@@ -21,4 +21,12 @@ func main() {
 	} else {
 		fmt.Printf("tidak lulus. nilai anda %d\n", point)
 	}
+
+	var names [4]string
+	names[0] = "trafalgar"
+	names[1] = "d"
+	names[2] = "water"
+	names[3] = "law"
+
+	fmt.Println(names[0], names[1], names[2], names[3])
 }
